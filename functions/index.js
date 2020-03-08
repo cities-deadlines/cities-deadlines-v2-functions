dd61365333240dc690953146077e414ce4fbe50a
alangshur <adlangshur@gmail.com>
2020-03-08 11:38:44 -0700

    Added base user functions

--- functions/index.js
+++ functions/index.js
@@ -5,3 +5,19 @@ var cors = require('cors')({ origin: true });
 // initialize app 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
+
+
+
+/*** USER FUNCTIONS ***/
+
+// add user to db upon creation
+exports.addUser = functions.auth.user().onCreate(user => {
+    return db.collection('users').doc(user.uid).set({
+        uid: user.uid
+    });
+});
+
+// remove user from db upon deletion
+exports.removeUser = functions.auth.user().onDelete(user => {
+    return db.collection('users').doc(user.uid).delete();
+});
